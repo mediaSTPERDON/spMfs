b4d34c2396a5eb5fe8c07039b23c4d777e1765f5
jf <jf@jf.jf>
2017-05-22 18:02:12 +0200

    ajout des donnees dans la liste des lecteurs via le composant modifier comboBoxEditable d'OTF²

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    global.schemas["Users"].find({}, function(err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        res.render('users', {title: 'Liste Users', users: result});
+    });
+
 });
 
 module.exports = router;
