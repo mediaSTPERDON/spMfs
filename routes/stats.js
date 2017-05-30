c7ae0366b78ab67aea98baf01b8ba5d9a458bc75
jf <jf@jf.jf>
2017-05-30 16:42:35 +0200

    fusion de code

--- routes/stats.js
+++ routes/stats.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var Chart = require('chart.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-	
-	res.render('stats', { title: 'Stats view', test: result });
-	
-	
-	console.log(req.body.myChart);
-});
+	res.render('stats', { title: 'Stats view' });
+	});
 
 
 module.exports = router;
