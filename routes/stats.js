47adc01ead6451640cf71ef44b5be305b1a7c900
cleve33 <rem861@hotmail.com>
2017-05-30 16:48:30 +0200

    correction bug date

--- routes/stats.js
+++ routes/stats.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var Chart = require('chart.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+	res.render('stats', { title: 'Stats view' });
 	
-	res.render('stats', { title: 'Stats view', test: result });
-	
-	
-	console.log(req.body.myChart);
 });
 
 
